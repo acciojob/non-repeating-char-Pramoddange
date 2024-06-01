537ab284ced5d156bf1d4ad7773171192f7318c3
Pramod Dange <123957464+Pramoddange@users.noreply.github.com>
2024-06-01 20:35:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
 	 let charCount = {};
-    for (let char of str) {
-        if (charCount[char] === undefined) {
-            charCount[char] = 1;
+    for (let i=0;i<str.length;i++) {
+        if (charCount[str.charAt(i)] === undefined) {
+            charCount[str.charAt(i)] = 1;
         } else {
-            charCount[char]++;
+            charCount[str.charAt(i)]++;
         }
     }
 	 for (let char of str) {
         if (charCount[char] === 1) {
             return char;
-			
+		
         }
     }
 	return null;
 }
-const input = prompt("Enter a string");
+/*const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
